583231b13c085676803f36d1c77e7fb9629ebdb6
Mehmet Ergun <mrmehmetergun@gmail.com>
2022-09-18 07:56:10 -0400

    Upcoming tab created

---
+++ Netflix/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix
+//
+//  Created by Mehmet Ergün on 2022-09-18.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
